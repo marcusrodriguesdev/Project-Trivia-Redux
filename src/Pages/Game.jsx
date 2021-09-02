6c8856a532acf70a2297f57a5f1ba96f51ec9a6d
Gustavo <gu.bressan@outlook.com>
2021-09-02 18:30:24 -0300

    Ajuste header

--- src/Pages/Game.jsx
+++ src/Pages/Game.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
+import Header from '../Components/Header';
 
 class Game extends React.Component {
   render() {
     return (
-      <header>
-        <img
-          src={ `https://www.gravatar.com/avatar/${JSON.parse(localStorage.getItem('ranking')).picture}` }
-          alt="Foto de Perfil"
-          data-testid="header-profile-picture"
-        />
-        <h1 data-testid="header-player-name">
-          {JSON.parse(localStorage.getItem('player')).name}
-        </h1>
-        <h2 data-testid="header-score">0</h2>
-      </header>
+      <Header />
     );
   }
 }
