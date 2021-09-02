9e4452fcf2ab6d2efc9fb1ec14bd6d692b69e8aa
Lucas Caribé <lucasmoc47@gmail.com>
2021-09-02 15:51:56 -0300

    Requisito 1

---
+++ src/pages/Login/index.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      email: '',
+      validName: false,
+      validEmail: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { value, name } = target;
+    const { validation } = target.dataset;
+
+    this.setState({
+      [name]: value,
+      [validation]: value.length > 0,
+    });
+  }
+
+  render() {
+    const { name, email, validName, validEmail } = this.state;
+
+    return (
+      <div>
+        <form>
+          <input
+            data-testid="input-player-name"
+            data-validation="validName"
+            placeholder="Nome"
+            type="text"
+            name="name"
+            value={ name }
+            onChange={ this.handleChange }
+          />
+
+          <input
+            data-testid="input-gravatar-email"
+            data-validation="validEmail"
+            placeholder="Email"
+            type="email"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+
+          <button
+            data-testid="btn-play"
+            type="submit"
+            disabled={ !validName || !validEmail }
+          >
+            Jogar
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
