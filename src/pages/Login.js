c88909c29cba7969c98a8f5b1f484f2e2d077f6c
Polyana Balotin <polybalotin@hotmail.com>
2021-09-02 16:39:48 -0300

    requisito 1

---
+++ src/pages/Login.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+export default class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      btnDisabled: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value },
+      () => this.disableBtn());
+  }
+
+  disableBtn() {
+    const { name, email } = this.state;
+    if (name.length > 0 && email.length > 0) {
+      this.setState({
+        btnDisabled: false,
+      });
+    } else {
+      this.setState({
+        btnDisabled: true,
+      });
+    }
+  }
+
+  render() {
+    const { btnDisabled } = this.state;
+    return (
+      <form>
+        <label htmlFor="input-player-name">
+          Nome :
+          <input
+            name="name"
+            type="text"
+            data-testid="input-player-name"
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <label htmlFor="input-gravatar-email">
+          Email :
+          <input
+            name="email"
+            type="text"
+            data-testid="input-gravatar-email"
+            onChange={ (event) => this.handleChange(event) }
+          />
+        </label>
+
+        <button
+          data-testid="btn-play"
+          type="button"
+          disabled={ btnDisabled }
+        >
+          Jogar
+        </button>
+      </form>
+    );
+  }
+}
