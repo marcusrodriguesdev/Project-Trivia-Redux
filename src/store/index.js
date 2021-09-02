d53b21f74f3ad00b515de332c86d13bd386c6cec
Alexsander-Trybe <Alexsander81002745@gmail.com>
2021-09-02 17:21:59 -0300

    Mais um teste

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
-import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
+import { createStore } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunk)),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
 if (window.Cypress) {
