bfd9a300d60b69d8f001d79fbaa6a7eed8fcac01
noel-srocha <manoelrocha.neto.93@hotmail.com>
2021-09-02 18:37:32 -0300

    Challenge 5 Completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
+import Quiz from './pages/Quiz';
 
 export default function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/quiz" component={ Quiz } />
         <Route exact path="/settings" component={ Settings } />
       </Switch>
     </div>
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import user from './user';
+import gravatar from './gravatar';
+import quiz from './quiz';
 
-const reducer = combineReducers({ user });
+const reducer = combineReducers({ user, gravatar, quiz });
 
 export default reducer;
