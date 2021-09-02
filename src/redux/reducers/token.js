9d548b79c47ecf70cc92802a8d729490b81ea0f4
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-09-02 19:28:13 -0300

    Requisito 2

---
+++ src/redux/reducers/token.js
@@ -0,0 +1,19 @@
+import { REQUEST_TOKEN_SUCCESS, REQUEST_TOKEN_ERROR } from '../actions/index';
+
+const INITIAL_STATE = {
+  token: {},
+  error: null,
+};
+
+const token = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case REQUEST_TOKEN_SUCCESS:
+    return { ...state, token: action.payload };
+  case REQUEST_TOKEN_ERROR:
+    return { ...state, error: action.payload };
+  default:
+    return state;
+  }
+};
+
+export default token;
