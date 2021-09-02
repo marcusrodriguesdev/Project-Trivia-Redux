6b6615045a138d26e36e698c165f00be9b6628b0
eaeniko <eaenikolai@gmail.com>
2021-09-02 17:15:46 -0300

    [req] REQ 1 concluido

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,33 @@ import logo from '../trivia.png';
 
 // requisito 1
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userValidation: true,
+      emailValidation: true,
+      user: '',
+      email: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { value, name } = event.target;
+    console.log(event.target.name);
+    this.setState({ [name]: value }, () => {
+      const { user, email } = this.state;
+      const userValidation = user === '';
+      const emailValidation = email === '';
+      this.setState({
+        userValidation,
+        emailValidation,
+      });
+    });
+  }
+
   render() {
+    const { emailValidation, userValidation } = this.state;
     return (
 
       <>
@@ -11,8 +37,35 @@ class Login extends React.Component {
         <h1>Login</h1>
         <form>
           <fieldset>
-            Xablau
-
+            <label htmlFor="user">
+              <input
+                id="user"
+                type="text"
+                name="user"
+                placeholder="User"
+                data-testid="input-player-name"
+                onChange={ this.handleChange }
+                required
+              />
+            </label>
+            <label htmlFor="email">
+              <input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Email"
+                data-testid="input-gravatar-email"
+                required
+                onChange={ this.handleChange }
+              />
+            </label>
+            <button
+              type="submit"
+              data-testid="btn-play"
+              disabled={ userValidation || emailValidation }
+            >
+              Entrar
+            </button>
           </fieldset>
         </form>
       </>
