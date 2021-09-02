6ca1c622831a5855f7182e628a7e45f07dd2aceb
rafael bamberg <raafaelbamberg@gmail.com>
2021-09-02 17:42:01 -0300

    req 2 incompleto

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import Login from './Pages/Login';
 import Settings from './Pages/Settings';
+import Game from './Pages/Game';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/game" component={ Game } />
       <Route path="/settings" component={ Settings } />
     </Switch>
   );
