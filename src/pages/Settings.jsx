bce183db4f36c88634acd88b4554c338f62bc1eb
marcus <marcus.rodrigues10@hotmail.com>
2021-09-02 18:55:17 -0300

    finish requirement 03

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Settings extends Component {
+  render() {
+    return (
+      <div>
+        <h1
+          className="settings-title"
+          data-testid="settings-title"
+        >
+          Settings
+        </h1>
+      </div>
+    );
+  }
+}
