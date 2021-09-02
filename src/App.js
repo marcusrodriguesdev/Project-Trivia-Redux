2e4b949d2abdb799505a35b9bd699ace47da1895
AlexanderCastro1 <82111064+AlexanderCastro1@users.noreply.github.com>
2021-09-02 20:00:38 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,17 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import login from './pages/login';
 import game from './pages/game';
 import ranking from './pages/ranking';
+import GameConfig from './pages/gameConfig';
 
 import './App.css';
-import GameConfig from './pages/gameConfig';
 
 export default function App() {
   return (
-    <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ login } />
         <Route path="/game" component={ game } />
         <Route path="/gameConfig" component={ GameConfig } />
         <Route path="/ranking" component={ ranking } />
       </Switch>
-    </BrowserRouter>
   );
 }
