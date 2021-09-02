29dde9a29080d18b70b4f92819072e9e5f279a9a
Guilherme <guilhermerg1234@gmail.com>
2021-09-02 20:26:40 -0300

    Requisito 2 Finalizado

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,19 @@
 export const LOGIN = 'LOGIN';
+export const TOKEN = 'TOKEN';
 
 export const login = (payload) => ({
   type: LOGIN,
   payload,
 });
+
+export const saveToken = (token) => ({
+  type: TOKEN,
+  token,
+});
+
+export const getToken = () => async (dispatch) => {
+  const response = await fetch('https://opentdb.com/api_token.php?command=request');
+  const objResponse = await response.json();
+  localStorage.setItem('token', objResponse.token);
+  dispatch(saveToken(objResponse));
+};
