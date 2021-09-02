41c804b0d4ac6d4d34ef5197c21c492d7e9ee227
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-09-02 17:06:36 -0300

    iniciando página do jogo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import logo from './trivia.png';
 import './App.css';
+import Game from './pages/Game';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
+      <Switch>
+        {/* <Route exact path="/" component={ Login } /> */}
+        <Route exact path="/game" component={ Game } />
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+          <p>
+            SUA VEZ
+          </p>
+        </header>
+      </Switch>
     </div>
   );
 }
