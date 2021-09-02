a6a9eb1f7cd6ac55e44f659d0bf8508ff220470e
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-09-02 16:07:55 -0300

    :white_check_mark: Corrigindo branch requisito 1
    Malss

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,7 +29,7 @@ class Login extends Component {
   render() {
     return (
       <div>
-        <h1>Login</h1>
+        <h1>Loginn</h1>
         <label htmlFor="email">
           Email:
           <input
