cdae7a316c152c8359c7a5ec0b2d94d6b48b4a25
Marcos Estevao <dusdoom@gmail.com>
2021-09-02 17:16:01 -0300

    requisito 3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Login extends React.Component {
   constructor() {
@@ -61,6 +62,11 @@ export default class Login extends React.Component {
         >
           Jogar
         </button>
+        <Link to="/config">
+          <button type="button" data-testid="btn-settings">
+            Configuracoes
+          </button>
+        </Link>
       </form>
     );
   }
