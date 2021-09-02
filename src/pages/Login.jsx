bfd98fd62e3c4ad31931b80e9adc6f369b9fb228
Alexsander-Trybe <Alexsander81002745@gmail.com>
2021-09-02 16:46:05 -0300

    Requisito 1 finalizado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,20 +2,58 @@ import React, { Component } from 'react';
 import logo from '../trivia.png';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      player: '',
+      email: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { handleChange } = this;
+    const { player, email } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={ logo } className="App-logo" alt="logo" />
         </header>
         <form>
-          <label htmlFor="name">
-            <input id="name" type="text" data-testid="input-player-name" />
+          <label htmlFor="palyer">
+            <input
+              id="player"
+              name="player"
+              value={ player }
+              onChange={ handleChange }
+              type="text"
+              data-testid="input-player-name"
+            />
           </label>
           <label htmlFor="email">
-            <input id="email" type="email" data-testid="input-gravatar-email" />
+            <input
+              id="email"
+              name="email"
+              value={ email }
+              onChange={ handleChange }
+              type="email"
+              data-testid="input-gravatar-email"
+            />
           </label>
-          <button type="button" data-testid="btn-play">
+          <button
+            type="button"
+            data-testid="btn-play"
+            disabled={ email.length && player.length ? '' : 'true' }
+          >
             Jogar
           </button>
         </form>
