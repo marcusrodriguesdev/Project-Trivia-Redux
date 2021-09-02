d4c727a5ef4d42611dfce08faa6959671a3ba8d2
Thales <thalessilva.tv@gmail.com>
2021-09-02 17:53:59 -0300

    estrutura requisito 13

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -8,17 +8,31 @@ class Feedback extends React.Component {
       picture: '',
       name: 'teste',
       score: 1,
+      message: '',
     };
   }
 
+  FeedbackMensage() {
+    const tryAgain = 'Podia ser melhor...';
+    const goodJob = 'Mandou bem!';
+    const MIN_SCORE = 3;
+    // recuperar score
+    if (score < MIN_SCORE) {
+      this.setState = ({ message: tryAgain });
+    } else {
+      this.setState = ({ message: goodJob });
+    }
+  }
+
   render() {
-    const { picture, name, score } = this.state;
+    const { picture, name, score, message } = this.state;
 
     return (
       <header>
         <img src={ picture } alt="foto de perfil" data-testid="header-profile-picture" />
         <h4 data-testid="header-player-name">{ name }</h4>
         <h6 data-testid="header-score">{ score }</h6>
+        <h2 data-testid="feedback-text">{ message }</h2>
       </header>
     );
   }
