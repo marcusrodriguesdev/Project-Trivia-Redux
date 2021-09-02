3f17a4c4255c58828cce35c29c6986a9652c0b22
JoãoBDf <joaobdf@hotmail.com>
2021-09-02 18:15:05 -0300

    Requisito 1 finalizado

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import logo from './trivia.png';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Login from './pages/Login';
+import Game from './pages/Game';
+import FeedBack from './pages/FeedBack';
+import Ranking from './pages/Ranking';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/game" component={ Game }>asd</Route>
+        <Route exact path="/FeedBack" component={ FeedBack }>asd</Route>
+        <Route exact path="/Ranking" component={ Ranking }>asd</Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/Game.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Game extends Component {
+  render() {
+    return (
+      <div>asdasd</div>
+    );
+  }
+}
+
+export default connect()(Game);
