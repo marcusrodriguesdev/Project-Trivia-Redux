c7fa0070d6c3d9a66ab87cef45f6fdab0f7c9d65
mateusparr <mateus.dji@icloud.com>
2021-09-02 17:35:46 -0300

    requisito 4

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,11 +1,32 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
 
 class Game extends Component {
   render() {
+    const { name, email } = this.props;
+    const hashEmail = md5(email).toString();
     return (
-      <div>Game</div>
+      <div>
+        <header>
+          <img src={ `https://www.gravatar.com/avatar/${hashEmail}` } alt="Gravatar" data-testid="header-profile-picture" />
+          <h2 data-testid="header-player-name">{ name }</h2>
+          <h2 data-testid="header-score">0</h2>
+        </header>
+      </div>
     );
   }
 }
 
-export default Game;
+Game.propTypes = {
+  email: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  name: state.login.user,
+  email: state.login.email,
+});
+
+export default connect(mapStateToProps, null)(Game);
