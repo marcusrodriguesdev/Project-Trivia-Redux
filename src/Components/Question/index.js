ae4741b99208dca3af5a09ad413b874c4f015a95
Joao Victor <joaogmavictor@gmail.com>
2021-09-03 18:38:28 -0300

    implementando Skeleto 9

--- src/Components/Question/index.js
+++ src/Components/Question/index.js
@@ -17,26 +17,27 @@ class Question extends React.Component {
   }
 
   handleClick({ target }) {
-    const { correctAnswer } = this.props;
+    const { correctAnswer, setTimeGlobal } = this.props;
     const AllButtons = document.querySelectorAll('button');
     AllButtons.forEach((button) => (correctAnswer === button.innerText
       ? button.classList.add('answer-correct')
       : button.classList.add('answer-wrong')));
-    if (target.innerText === 'correct-answer') { this.calcPonts(); }
+    if (target.innerText === correctAnswer) { this.calcPonts(); }
+    setTimeGlobal(true);
   }
 
   calcPonts() {
     const { difficulty } = this.props;
+    const timer = document.querySelector('#timer').innerText;
     const pontDifficulty = {
       hard: 3,
       medium: 2,
       easy: 1,
     };
-    // Para terminar a implementação desse requisito é necesssario o timer
-    // const total = 10 + (timer * pontDifficulty[difficulty]);
-    // const stateLocal = JSON.parse(localStorage.getItem('state'));
-    // const newLocal = { ...stateLocal, score: total };
-    // localStorage.setItem('state', JSON.stringify(newLocal));
+    const total = 10 + (timer * pontDifficulty[difficulty]);
+    const stateLocal = JSON.parse(localStorage.getItem('state'));
+    const newLocal = { ...stateLocal, score: total };
+    localStorage.setItem('state', JSON.stringify(newLocal));
   }
 
   render() {
@@ -99,7 +100,6 @@ const MapStateToProps = (state) => ({
 });
 
 const MapDispachToProps = (dispatch) => ({
-
   setTimeGlobal: (payload) => dispatch(setTimer(payload)),
 });
 
