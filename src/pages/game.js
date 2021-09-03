0ba991fdd0275069b4c6f4ef7de6da1d41992823
Mariana Garcia <maridgarcia045@gmail.com>
2021-09-03 10:29:01 -0300

    Requisito 5 - em andamento

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
 
 class game extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {},
+    };
+
+    this.fetchAPI = this.fetchAPI.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchAPI();
+  }
+
+  async fetchAPI() {
+    const response = await fetch('https://opentdb.com/api.php?amount=5');
+    const data = await response.json();
+    this.setState({
+      data: data.results[0],
+    });
+    return data;
+  }
+
   render() {
+    const { data } = this.state;
     return (
       <div className="App">
-        tela do jogo
+        Tela de jogo
+        <div className="question-board">
+          <h1 data-testid="question-category">{data.category}</h1>
+          <h2 data-testid="question-text">{data.question}</h2>
+          <h3>{data.incorrect_answers}</h3>
+          {/* <ol>
+            {data.incorrect_answers
+              .map(((answer, index) => (
+                <li key={ index }>
+                  <button
+                    type="button"
+                    data-testid={ `wrong-answer${index}` }
+                  >
+                    { answer }
+                  </button>
+                </li>
+              )))}
+          </ol> */}
+        </div>
       </div>
     );
   }
