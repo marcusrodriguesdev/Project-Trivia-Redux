a4f244d53c0e781d17088541bcfa57c22413e4a7
Leonardo <leo_mc_inter@hotmail.com>
2021-09-03 20:24:29 -0300

    Teste

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -10,7 +10,7 @@ class Game extends React.Component {
   constructor() {
     super();
     this.state = {
-      questions: {},
+      questions: { results: [] },
       loading: true,
       countQuestion: 0,
     };
