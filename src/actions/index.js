400720058c554398de103085b6eb2da8feb6e9e6
Jonmorais <jonathan_ms15@hotmail.com>
2021-09-03 16:31:45 -0300

    Requisito 5 + 6

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,13 @@
 const LOGIN = 'LOGIN';
 
-export default function Login(name, email, profile) {
+export default function Login(name, email, profile, token) {
   return ({
     type: LOGIN,
     payload: {
       name,
       email,
       profile,
+      token,
     },
   });
 }
