c8e8594c246ccb5c61082ba9801a517c2ce87807
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-09-03 14:46:32 -0300

    Cria página de configurações

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Settings extends React.Component {
+  render() {
+    return (
+      <div>
+        <p data-testid="settings-title">
+          Pagina de Configuração
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Settings;
