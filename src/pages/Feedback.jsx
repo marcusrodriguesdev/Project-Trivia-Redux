c31f8a8b422a64d518a6cbac5698826d97803084
Leonardo <leo_mc_inter@hotmail.com>
2021-09-03 15:54:29 -0300

    Requisito 12

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Feedback extends React.Component {
+  render() {
+    const { avatar, name, score } = this.props;
+    return (
+      <header>
+        <div data-testid="header-profile-picture">{avatar}</div>
+        <p data-testid="header-player-name">{name}</p>
+        <span data-testid="header-score">{score}</span>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  avatar: state.player.name,
+  name: state.player.email,
+  score: state.player.score,
+});
+
+Feedback.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps)(Feedback);
