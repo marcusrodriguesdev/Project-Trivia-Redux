e9211973ff484d847e8c18bb2ead58ea2a6712f2
unknown <brunodevmoraes@gmail.com>
2021-09-03 16:59:25 -0300

    Resolve conflitos

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -1,15 +1,23 @@
 import { SAVE_QUESTIONS } from '../actions';
 
 const INITIAL_STATE = {
-  questions: {},
+  questions: [{
+    category: '',
+    type: '',
+    difficulty: '',
+    question: '',
+    correct_answer: '',
+    incorrect_answers: [],
+  }],
 };
 
 export default function game(state = INITIAL_STATE, action) {
   switch (action.type) {
   case SAVE_QUESTIONS:
+    console.log('cheguei aqui ein');
     return {
       ...state,
-      questions: action.payload,
+      questions: action.payload.results,
     };
   default:
     return state;
