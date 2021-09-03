300706867b7a64156b86ec0872cea9de866b163b
Felipe Carvalho <fecarvalho066@gmail.com>
2021-09-03 10:18:03 -0300

    Inicio do Requisito 5

--- src/Pages/Game.jsx
+++ src/Pages/Game.jsx
@@ -1,12 +1,58 @@
 import React from 'react';
 import Header from '../Components/Header';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
 
 class Game extends React.Component {
   render() {
     return (
+      <>
       <Header />
+      <fieldset>
+      <h1
+        data-testid="question-category"
+      >
+        {data.results[0].category}
+      </h1>
+      <h2
+        data-testid="question-text"
+      >
+        {data.results[0].question}
+      </h2>
+      <ol>
+        { data.results[0].incorrect_answers
+          .map(((answer, index) => (
+            <li key={ index }>
+              <button
+                data-testid={ `wrong-answer-${index}` }
+                type="button"
+              >
+                { answer }
+              </button>
+            </li>
+          ))) }
+        <li>
+          <button
+            type="button"
+            data-testid="correct-answer"
+          >
+            {data.results[0].correct_answer}
+          </button>
+        </li>
+      </ol>
+    </fieldset>
+  </>
     );
   }
 }
 
-export default Game;
+const mapStateToProps = (state) => ({
+  token: state.login.token,
+});
+
+Game.propTypes = {
+  token: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Game);
