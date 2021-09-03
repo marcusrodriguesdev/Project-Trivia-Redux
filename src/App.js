70fe10aa42a021c2c9b91e27ab777b644ee843d8
Guilherme Giacomini Teixeira <ggiacomini2012@gmail.com>
2021-09-03 18:23:09 -0300

    req 16 refatorado

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router';
+import Login from './pages/Login';
 import Settings from './pages/Settings';
 import Game from './pages/Game';
-import Login from './pages/Login';
 import Ranking from './pages/Ranking';
 
 export default function App() {
   return (
     <div>
       <Switch>
-
         <Route exact path="/" component={ Login } />
         <Route exact path="/settings" component={ Settings } />
         <Route exact path="/play" component={ Game } />
         <Route exact path="/ranking" component={ Ranking } />
-
       </Switch>
     </div>
   );
