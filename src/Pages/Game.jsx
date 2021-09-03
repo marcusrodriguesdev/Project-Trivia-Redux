4ef1dffdc5087e4a849c550839e86cb7e03dda11
Felipe Carvalho <fecarvalho066@gmail.com>
2021-09-03 10:47:15 -0300

    Requisito 5 - Implementa fieldset

--- src/Pages/Game.jsx
+++ src/Pages/Game.jsx
@@ -1,58 +1,30 @@
 import React from 'react';
 import Header from '../Components/Header';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-
 
 class Game extends React.Component {
   render() {
     return (
       <>
-      <Header />
-      <fieldset>
-      <h1
-        data-testid="question-category"
-      >
-        {data.results[0].category}
-      </h1>
-      <h2
-        data-testid="question-text"
-      >
-        {data.results[0].question}
-      </h2>
-      <ol>
-        { data.results[0].incorrect_answers
-          .map(((answer, index) => (
-            <li key={ index }>
-              <button
-                data-testid={ `wrong-answer-${index}` }
-                type="button"
-              >
-                { answer }
+        <Header />
+        <fieldset>
+          <h1 data-testid="question-category">teste</h1>
+          <h2 data-testid="question-text">teste</h2>
+          <ol>
+            <li>
+              <button data-testid="wrong-answer" type="button">
+                teste
               </button>
             </li>
-          ))) }
-        <li>
-          <button
-            type="button"
-            data-testid="correct-answer"
-          >
-            {data.results[0].correct_answer}
-          </button>
-        </li>
-      </ol>
-    </fieldset>
-  </>
+            <li>
+              <button type="button" data-testid="correct-answer">
+                teste
+              </button>
+            </li>
+          </ol>
+        </fieldset>
+      </>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  token: state.login.token,
-});
-
-Game.propTypes = {
-  token: PropTypes.string.isRequired,
-};
-
-export default connect(mapStateToProps)(Game);
+export default Game;
