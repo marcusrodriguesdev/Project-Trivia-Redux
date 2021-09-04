b283c9f5654462bdd62429e0b8e3056cdc88aa98
PedroSpelta <p.h.s.8989@hotmail.com>
2021-09-04 06:26:22 -0300

    terminando o requisito 10

--- src/components/Answer/index.js
+++ src/components/Answer/index.js
@@ -14,8 +14,9 @@ class Answer extends Component {
       guess,
       timeOver,
       checkAnswer,
-      questionInfo: { difficulty },
+      questionInfo,
     } = this.props;
+    const { difficulty } = questionInfo;
 
     const testId = answer.isCorrect
       ? 'correct-answer'
@@ -46,14 +47,18 @@ class Answer extends Component {
 }
 
 Answer.propTypes = {
-  timeOver: PropTypes.bool.isRequired,
   answer: PropTypes.shape({
     isCorrect: PropTypes.bool.isRequired,
     text: PropTypes.string.isRequired,
   }).isRequired,
-  index: PropTypes.number.isRequired,
-  guessed: PropTypes.bool.isRequired,
+  checkAnswer: PropTypes.func.isRequired,
   guess: PropTypes.func.isRequired,
+  guessed: PropTypes.bool.isRequired,
+  index: PropTypes.number.isRequired,
+  questionInfo: PropTypes.shape({
+    difficulty: PropTypes.string.isRequired,
+  }).isRequired,
+  timeOver: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = ({ game }) => ({
