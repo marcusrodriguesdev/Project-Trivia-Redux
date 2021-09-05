36d998a0ec446c9581d37172b8692b1c35e88916
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-09-05 11:55:23 -0400

    requisito 18

--- src/Pages/Ranking.js
+++ src/Pages/Ranking.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
+  handleClick() {
+    const { history } = this.props;
+
+    history.push('/');
+  }
+
   render() {
     return (
       <div>
         <h2 data-testid="ranking-title">Ranking</h2>
+        <main>
+          <button
+            type="button"
+            data-testid="btn-go-home"
+            onClick={ this.handleClick.bind(this) }
+          >
+            Home
+          </button>
+        </main>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Ranking;
