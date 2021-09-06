197e75bf36268b04c4b039dd3f6956c34e4751f2
Lucas T Franca <dev.lucasteixeira@gmail.com>
2021-09-06 10:51:08 -0300

    Requisito 14 Finalizado

--- src/pages/FeedBack.jsx
+++ src/pages/FeedBack.jsx
@@ -4,22 +4,28 @@ import PropTypes from 'prop-types';
 
 class FeedBack extends React.Component {
   render() {
-    const { assertions } = this.props;
+    const { assertions, score } = this.props;
     const FEEDBACK_NUMBER = 3;
     return (
       <div data-testid="feedback-text">
         { assertions < FEEDBACK_NUMBER ? 'Podia ser melhor...' : 'Mandou bem!' }
+        <p>
+          <span data-testid="feedback-total-score">{ score }</span>
+          <span data-testid="feedback-total-question">{ assertions }</span>
+        </p>
       </div>
     );
   }
 }
 
 FeedBack.propTypes = {
-  assertions: PropTypes.string,
+  assertions: PropTypes.number,
+  score: PropTypes.number,
 }.isRequired;
 
 const mapStateToProps = ({ player }) => ({
   assertions: player.assertions,
+  score: player.score,
 });
 
 export default connect(mapStateToProps)(FeedBack);
