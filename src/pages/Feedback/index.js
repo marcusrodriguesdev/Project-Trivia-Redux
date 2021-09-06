7c06cd68f581e827f80644b0843e3899dc4bf417
Lucas Caribé <lucasmoc47@gmail.com>
2021-09-05 22:48:37 -0300

    Requisito 13

--- src/pages/Feedback/index.js
+++ src/pages/Feedback/index.js
@@ -4,11 +4,23 @@ import { connect } from 'react-redux';
 import Header from '../../components/Header';
 
 class Feedback extends Component {
+  getAssertions() {
+    const local = window.localStorage.getItem('state');
+    const parsedLocal = JSON.parse(local);
+
+    return parsedLocal.player.assertions;
+  }
+
   render() {
+    const MIN_ASSERTIONS = 3;
+    const assertions = this.getAssertions();
+
     return (
       <div>
         <Header />
-        <p data-testid="feedback-text">Feedback</p>
+        <p data-testid="feedback-text">
+          {assertions >= MIN_ASSERTIONS ? 'Mandou bem!' : 'Podia ser melhor...'}
+        </p>
       </div>
     );
   }
