21b21a0e91428877e75af9bcf24fa9e6f09969ca
PedroSpelta <p.h.s.8989@hotmail.com>
2021-09-06 23:43:13 -0300

    Finalizando o ranking

--- src/pages/Ranking/index.js
+++ src/pages/Ranking/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 class Ranking extends Component {
@@ -8,6 +9,8 @@ class Ranking extends Component {
     };
 
     this.setPlayers = this.setPlayers.bind(this);
+    this.handleHomeClick = this.handleHomeClick.bind(this);
+    this.sortPlayers = this.sortPlayers.bind(this);
   }
 
   componentDidMount() {
@@ -17,7 +20,17 @@ class Ranking extends Component {
   setPlayers() {
     const localPlayersString = window.localStorage.getItem('ranking');
     const localPlayers = JSON.parse(localPlayersString);
-    this.setState({ players: localPlayers });
+    const sortedPlayers = this.sortPlayers(localPlayers);
+    this.setState({ players: sortedPlayers });
+  }
+
+  sortPlayers(players) {
+    return players.sort((a, b) => b.score - a.score);
+  }
+
+  handleHomeClick() {
+    const { history } = this.props;
+    history.push('/');
   }
 
   render() {
@@ -28,13 +41,28 @@ class Ranking extends Component {
         {players.map((player, index) => (
           <div key={ index }>
             <img src={ player.picture } alt="foto de perfil" />
-            <h3 key={ index } data-testid={ `player-name-${index}` }>{player.name}</h3>
+            <h3 key={ index } data-testid={ `player-name-${index}` }>
+              {player.name}
+            </h3>
             <h3 data-testid={ `player-score-${index}` }>{player.score}</h3>
           </div>
         ))}
+        <button
+          type="button"
+          onClick={ this.handleHomeClick }
+          data-testid="btn-go-home"
+        >
+          Home
+        </button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Ranking;
