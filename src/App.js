5c6fcebe6bdfa0d099b3b652c4ac3b65287ec40c
Vini Queiroz <viniderp@gmail.com>
2021-09-08 19:02:18 -0300

    Req 5-6

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router';
 import './App.css';
 import Configuration from './pages/Configuração';
 import Login from './pages/Login';
+import Game from './pages/Game';
 
 export default function App() {
   return (
     <Switch>
       <Route path="/configuration" component={ Configuration } />
       <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+      <Route exact path="/game" render={ (props) => <Game { ...props } /> } />
     </Switch>
   );
 }
