75c2e926b10945d405f396bf3991e794268295ad
Aleilton Chavenco <aleilton@gmail.com>
2021-09-07 23:26:17 -0300

    Requisito 12 - Commit da vitóriagit add .!

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const { name, avatar, score } = this.props;
     return (
       <div>
+        <Header name={ name } avatar={ avatar } score={ score } />
         <p data-testid="feedback-text">
           Feedback
         </p>
@@ -12,4 +18,16 @@ class Feedback extends Component {
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (
+  { player: { playerInfo: { name, avatar }, score } },
+) => ({
+  name, avatar, score,
+});
+
+export default connect(mapStateToProps)(Feedback);
