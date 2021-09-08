7a0f403712f81f235e52fa1e18431f224c5500b1
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-09-08 18:26:54 -0300

    Requisito 13 o/

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -3,13 +3,23 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+  //     // this.getFeedback = this.getFeedback.bind(this);
+  //   }
+
   render() {
     const player = JSON.parse(localStorage.getItem('state'));
-    console.log(player);
+    const MIN_ASSERTIONS = 3;
+
+    const getFeedback = () => (player.player.assertions < MIN_ASSERTIONS
+      ? 'Podia ser melhor...'
+      : 'Mandou bem!');
     return (
       <>
-        <span data-testid="feedback-text">Feedback</span>
         <Header score={ player.player.score } />
+        <span data-testid="feedback-text">{ getFeedback() }</span>
       </>
     );
   }
