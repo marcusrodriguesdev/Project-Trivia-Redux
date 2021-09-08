bcc3b9be27c9c55e24f9ec2d01509ece5272163d
Barros99 <jeanmeneses@hotmail.com>
2021-09-08 16:53:04 -0300

    requisito 13

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -3,16 +3,18 @@ import React, { Component } from 'react';
 export default class Feedback extends Component {
   render() {
     const userData = JSON.parse(localStorage.getItem('state'));
-    console.log(userData);
+    const NUMBER = 3;
     const { player } = userData;
     return (
       <div data-testid="feedback-text">
         <h1 data-testid="header-player-name">{player.name}</h1>
-        <img data-testid="header-profile-picture" src={ player.picture } alt="avatar" />
-        <p data-testid="header-score">
-          {player.score}
-        </p>
-
+        <img
+          data-testid="header-profile-picture"
+          src={ player.picture }
+          alt="avatar"
+        />
+        <p data-testid="header-score">{player.score}</p>
+        {player.assertions < NUMBER ? 'Podia ser melhor...' : 'Mandou bem!'}
       </div>
     );
   }
