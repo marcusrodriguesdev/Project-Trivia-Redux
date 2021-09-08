3820b4149c65223ae5c074bf7f6f6d7edf36bdf0
Rodrigo Cesar Destro Fidencio <60055837+rodrigocdfidencio@users.noreply.github.com>
2021-09-08 18:59:42 -0300

    [GRUPO 1] - Requisito 15

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Feedback extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.playAgain = this.playAgain.bind(this);
+  }
+
+  playAgain() {
+    console.log('Jogar novamente');
+  }
+
   render() {
     const { player: { assertions, score } } = JSON.parse(localStorage.getItem('state'));
     const minAssertions = 3;
@@ -17,6 +28,15 @@ class Feedback extends React.Component {
           Seu score final é:
           <span data-testid="feedback-total-score">{ score }</span>
         </p>
+        <Link to="/">
+          <button
+            data-testid="btn-play-again"
+            type="button"
+            onClick={ this.playAgain }
+          >
+            Jogar Novamente
+          </button>
+        </Link>
       </div>
     );
   }
