ea62baf1ec545d03e0f9088a0f7d507319908cc2
marcus <marcus.rodrigues10@hotmail.com>
2021-09-08 18:48:05 -0300

    Requirement 13, 15 and 16

--- src/pages/FeedBack.jsx
+++ src/pages/FeedBack.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class FeedBack extends React.Component {
   render() {
-    const { history } = this.props;
+    const { history, assertions } = this.props;
+    const checkAssertion = () => {
+      const NUMBER_THREE = 3;
+      if (assertions < NUMBER_THREE) {
+        return <div>Podia ser melhor...</div>;
+      } if (assertions >= NUMBER_THREE) {
+        return <div>Mandou bem!</div>;
+      }
+    };
     return (
-      <div data-testid="feedback-text">
+      <div>
         FeedBack
         <div>
           <Header />
@@ -25,14 +34,23 @@ class FeedBack extends React.Component {
             Ver ranking
           </button>
         </div>
+        <div data-testid="feedback-text">
+          { checkAssertion() }
+        </div>
       </div>
     );
   }
 }
+
 FeedBack.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func,
   }).isRequired,
+  assertions: PropTypes.number.isRequired,
 };
 
-export default FeedBack;
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+});
+
+export default connect(mapStateToProps, null)(FeedBack);
