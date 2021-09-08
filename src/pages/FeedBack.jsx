b1aebbae3aaf0b2fed92e58ee3020148cf758a4c
bvottri <bruna.votre@gmail.com>
2021-09-08 18:20:52 -0300

    Requirement 15

--- src/pages/FeedBack.jsx
+++ src/pages/FeedBack.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class FeedBack extends React.Component {
   render() {
+    const { history } = this.props;
     return (
       <div data-testid="feedback-text">
         FeedBack
         <div>
           <Header />
+          <button
+            onClick={ () => history.push('/') }
+            type="button"
+            data-testid="btn-play-again"
+          >
+            Jogar novamente
+          </button>
         </div>
       </div>
     );
   }
 }
+FeedBack.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default FeedBack;
