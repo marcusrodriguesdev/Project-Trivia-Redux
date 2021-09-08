a83b05744b7148129fb1f19970e89ae69180c92e
Pedro <pedrorsehn@hotmail.com>
2021-09-08 17:36:17 -0300

    add ex15 completo

--- src/pages/FeedbackPage/index.jsx
+++ src/pages/FeedbackPage/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import md5 from 'crypto-js/md5';
+import { Link } from 'react-router-dom';
 
 class FeedbackPage extends React.Component {
   constructor() {
@@ -42,6 +43,9 @@ class FeedbackPage extends React.Component {
         />
         <div data-testid="header-score">{ score }</div>
         <div data-testid="feedback-text">{ this.handleFeedbackMsg(assertions) }</div>
+        <Link to="/">
+          <button data-testid="btn-play-again" type="button"> Jogar Novamente </button>
+        </Link>
       </>
     );
   }
