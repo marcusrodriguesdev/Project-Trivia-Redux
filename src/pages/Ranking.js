a1b361a1b73c333f4d45108bbc33ed07f1a16a29
Henrique Remedios <henriqueremedios@gmail.com>
2021-09-08 16:25:39 -0300

    17. Crie a tela de ranking

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,12 +1,61 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class Ranking extends Component {
+class Ranking extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ranking: [{
+        name: '',
+        score: '',
+      }],
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.showRanking = this.showRanking.bind(this);
+  }
+
+  componentDidMount() {
+    const getUserRanking = JSON.parse(localStorage.getItem('state'));
+    const { player: { score, name } } = getUserRanking;
+    this.showRanking(name, score);
+  }
+
+  showRanking(name, score) {
+    this.setState((prevState) => ({ ranking: [...prevState.ranking, { name, score }] }));
+  }
+
+  handleClick() {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
+    const { ranking } = this.state;
     return (
       <div>
-        <h2 data-testid="ranking-title">Ranking</h2>
-      </div>);
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <ul>
+          { ranking.length === 0 ? undefined : ranking.map((user) => (
+            <li key={ user }>
+              <span>{user.name}</span>
+              <span>{user.score}</span>
+            </li>
+          ))}
+        </ul>
+        <button
+          type="button"
+          onClick={ this.handleClick }
+          data-testid="btn-go-home"
+        >
+          Inicio
+        </button>
+      </div>
+    );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.objectOf(PropTypes.object).isRequired,
+};
 
 export default Ranking;
