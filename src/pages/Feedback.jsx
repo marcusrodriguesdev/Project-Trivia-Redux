1602bbafd731a94d3e11bd41f138ac0893910cbd
Thaís Sampaio <thais_castrocs@outlook.com>
2021-09-09 16:26:22 -0300

    [Group 5]Requisito 14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -10,22 +10,38 @@ class Feedback extends React.Component {
     const storageInfos = localStorage.getItem('state');
     const scorePlayer = JSON.parse(storageInfos);
     return (
-      <header>
-        <h1 data-testid="feedback-text">Feedbacks</h1>
-        <div data-testid="header-profile-picture">{avatar}</div>
-        <p data-testid="header-player-name">{name}</p>
-        <div data-testid="header-score">{scorePlayer.player.score}</div>
-        {scorePlayer.player.assertions < tres ? (
-          <p data-testid="feedback-text">Podia ser melhor...</p>
-        ) : (
-          ''
-        )}
-        {scorePlayer.player.assertions >= tres ? (
-          <p data-testid="feedback-text">Mandou bem!</p>
-        ) : (
-          ''
-        )}
-      </header>
+      <div>
+        <header>
+          <h1 data-testid="feedback-text">Feedbacks</h1>
+          <div data-testid="header-profile-picture">{avatar}</div>
+          <p data-testid="header-player-name">{name}</p>
+          <div data-testid="header-score">{scorePlayer.player.score}</div>
+          {scorePlayer.player.assertions < tres ? (
+            <p data-testid="feedback-text">Podia ser melhor...</p>
+          ) : (
+            ''
+          )}
+          {scorePlayer.player.assertions >= tres ? (
+            <p data-testid="feedback-text">Mandou bem!</p>
+          ) : (
+            ''
+          )}
+        </header>
+        <section>
+          <h4>
+            Pontuação Final:
+          </h4>
+          <span data-testid="feedback-total-score">
+            {scorePlayer.player.score}
+          </span>
+          <h4>
+            Número de questões corretas:
+          </h4>
+          <span data-testid="feedback-total-question">
+            {scorePlayer.player.assertions}
+          </span>
+        </section>
+      </div>
     );
   }
 }
