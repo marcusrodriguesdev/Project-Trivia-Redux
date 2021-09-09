34db662a1ec4a5b1ed1a13cab323f428fd5627fa
MilenaCLeme <milenaleme4@hotmail.com>
2021-09-09 16:41:29 -0300

    Ranking req 18

--- src/pages/ranking.js
+++ src/pages/ranking.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class ranking extends Component {
   render() {
     return (
       <div className="App">
         <h1 data-testid="ranking-title">Ranking</h1>
+        <Link to="/">
+          <button type="submit" data-testid="btn-go-home">Voltar a tela de login</button>
+        </Link>
       </div>
     );
   }
