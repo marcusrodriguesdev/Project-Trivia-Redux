5fc3aedd201df57247f86c8099947f72ba8b2761
Jonmorais <jonathan_ms15@hotmail.com>
2021-09-09 16:00:03 -0300

    Requisito 12

--- src/Pages/Feedback/index.js
+++ src/Pages/Feedback/index.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Feedback extends Component {
+class Feedback extends Component {
   render() {
+    const { dados } = this.props;
+    const { name, profile, score } = dados;
     return (
       <div>
-        <span data-testid="feedback-text">Feedback</span>
+        <header>
+          <span data-testid="feedback-text">Feedback</span>
+          <p data-testid="header-player-name">{ name }</p>
+          <p data-testid="header-score">{ score }</p>
+          <img
+            src={ `https://www.gravatar.com/avatar/${profile}` }
+            alt="gravatar"
+            data-testid="header-profile-picture"
+          />
+        </header>
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  dados: state.scoreReducer,
+});
+
+export default connect(mapStateToProps, null)(Feedback);
+
+Feedback.propTypes = {
+  dados: PropTypes.objectOf(PropTypes.string).isRequired,
+};
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import loginReducer from './login';
+import scoreReducer from './score';
 
 const rootReducer = combineReducers({
   loginReducer,
-
+  scoreReducer,
 });
 
 export default rootReducer;
