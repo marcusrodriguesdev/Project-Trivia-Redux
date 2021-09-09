5b6aaff5e3559a2c60a438244b32aa58aa1ed12c
gustavoalves23 <gustavo.alves388@gmail.com>
2021-09-09 17:05:18 -0300

    adicao css

--- src/Pages/Ranking.jsx
+++ src/Pages/Ranking.jsx
@@ -6,17 +6,19 @@ class Ranking extends Component {
     let ranking = JSON.parse(localStorage.getItem('ranking'));
     ranking = ranking.sort((value1, value2) => value2.score - value1.score);
     return (
-      <div>
+      <div className="ranking-div">
         <header data-testid="ranking-title">
           Ranking
         </header>
-        { ranking.map((match, index) => (
-          <div key={ index }>
-            <img src={ `https://www.gravatar.com/avatar/${match.picture}` } alt={ match.name } />
-            <h3 data-testid={ `player-name-${index}` }>{ match.name }</h3>
-            <h2 data-testid={ `player-score-${index}` }>{ match.score }</h2>
-          </div>
-        ))}
+        <div className="rankings">
+          { ranking.map((match, index) => (
+            <div className="each-ranking" key={ index }>
+              <img src={ `https://www.gravatar.com/avatar/${match.picture}` } alt={ match.name } />
+              <h3 data-testid={ `player-name-${index}` }>{ match.name }</h3>
+              <h2 data-testid={ `player-score-${index}` }>{ match.score }</h2>
+            </div>
+          ))}
+        </div>
         <Link to="/">
           <button type="button" data-testid="btn-go-home">Jogar novamente</button>
         </Link>
