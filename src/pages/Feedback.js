5d88239de03a8c88a8d9a4467853167372c8faed
Camila Dornas <camiladornas@gmail.com>
2021-09-09 14:54:53 -0300

    Requisito 13

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -4,16 +4,25 @@ import Header from '../components/Header';
 
 export default class Feedback extends Component {
   render() {
-    const { score } = this.props;
+    const { player } = this.props;
+    const { score, assertions } = player;
+    const THREE = 3;
     return (
       <div>
         <Header score={ score } />
-        <span data-testid="feedback-text">feedback</span>
+        <span data-testid="feedback-text">
+          {
+            assertions >= THREE ? 'Mandou bem!' : 'Podia ser melhor...'
+          }
+        </span>
       </div>
     );
   }
 }
 
 Feedback.propTypes = {
-  score: PropTypes.number.isRequired,
+  player: PropTypes.shape({
+    score: PropTypes.number,
+    assertions: PropTypes.number,
+  }).isRequired,
 };
