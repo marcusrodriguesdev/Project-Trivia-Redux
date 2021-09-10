d1a31d000605097c20e85b58e1ebe4676f374487
Alexander <alexanderteodorocastro95@hotmail.com>
2021-09-09 23:57:03 -0300

    test

--- src/pages/ranking.js
+++ src/pages/ranking.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 
 class ranking extends Component {
   render() {
+    const players = JSON.parse(localStorage.ranking)
+      .short((a, b) => (b.score - a.score));
     return (
-      <div className="App">
-        <h1 data-testid="ranking-title">Ranking</h1>
-      </div>
+      <fieldset className="App">
+        <div className="App">
+          <h2 data-testid="ranking-title">Ranking</h2>
+        </div>
+
+        {players.map(({ name, picture, score }, i) => (
+          <div key={ i }>
+            <span>{i + 1}</span>
+            <span>{' - '}</span>
+            <img src={ picture } alt="PlayerImage" />
+            <span data-testid={ `player-name-${index}` }>{name}</span>
+            <span>{' - '}</span>
+            <span data-testid={ `player-score-${index}` }>{score}</span>
+          </div>
+        ))}
+      </fieldset>
     );
   }
 }
