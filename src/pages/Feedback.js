ba740a32724d0eca3bfab4fd43a39201c3f41ecf
Vini Queiroz <viniderp@gmail.com>
2021-09-09 21:40:01 -0300

    Requisito 11

---
+++ src/pages/Feedback.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Feedback extends React.Component {
+  render() {
+    return (
+      <div>
+        <p data-testid="feedback-text">TELA DE FEEDBACK</p>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
