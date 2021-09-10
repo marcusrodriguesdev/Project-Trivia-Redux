f0556c0d5b320a95844e5445f1c8f18029524b9c
JoãoBDf <joaobdf@hotmail.com>
2021-09-10 10:29:19 -0300

    pull 16

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 class Ranking extends Component {
   render() {
     return (
-      <div>asdasd</div>
+      <h1 data-testid="ranking-title">Ranking</h1>
     );
   }
 }
