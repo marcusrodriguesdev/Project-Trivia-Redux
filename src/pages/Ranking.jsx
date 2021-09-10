0566a2d9eed9ae2d8f95649d8f2a596daf28ce21
JoãoBDf <joaobdf@hotmail.com>
2021-09-10 10:38:04 -0300

    Requisito 18 Finalizado

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <div>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="btn-go-home"
+          >
+            Voltar ao início
+          </button>
+        </Link>
+      </div>
     );
   }
 }
