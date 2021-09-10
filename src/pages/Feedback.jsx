e6c99895359706a75e62c1d0ed9432123fdb35f3
Letícia Castro <aicitelks@gmail.com>
2021-09-10 16:58:49 -0300

    Requisito 17 | ranking montado e ordenado pela pontuação

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -3,8 +3,28 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 const tres = 3;
+const RANKING_KEY = 'ranking';
 
 class Feedback extends React.Component {
+  componentDidMount() {
+    this.setLocalStorage();
+  }
+
+  setLocalStorage() {
+    // obter os dados do estado global
+    const { name, score } = this.props;
+    // constante que será o array a ser armazenado no local storage
+    // na tribuição dessa constate temos o seguinte:
+    // - se já existir a chave RANKING_KEY, a constante recebe o local storage já convertido em objeto/array
+    // - senão ela recebe um array vazio
+    // o fato de ter colocado um '||' torna essa atribuição possível
+    const rankings = JSON.parse(localStorage.getItem(RANKING_KEY)) || [];
+    // dar o push do estado global nesse array
+    rankings.push({ name, score });
+    // setar esse array no local storage
+    localStorage.setItem(RANKING_KEY, JSON.stringify(rankings));
+  }
+
   render() {
     const { avatar, name, history } = this.props;
     const storageInfos = localStorage.getItem('state');
@@ -66,6 +86,7 @@ const mapStateToProps = (state) => ({
 Feedback.propTypes = {
   avatar: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
   history: PropTypes.objectOf().isRequired,
 };
 
--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+const RANKING_KEY = 'ranking';
+const one = -1;
 
 class Ranking extends React.Component {
   render() {
+    const rankings = JSON.parse(localStorage.getItem(RANKING_KEY));
+    // SOURCE: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+    rankings.sort((a, b) => {
+      if (a.score > b.score) {
+        return one;
+      }
+      if (a.score < b.score) {
+        return 1;
+      }
+      // a must be equal to b
+      return 0;
+    });
+
     return (
       <main>
         <h1 data-testid="ranking-title">Ranking</h1>
+        { rankings.map(({ name, score }, index) => (
+          <div key={ index }>
+            <span data-testid={ `player-name-${index}` }>{ name }</span>
+            <span data-testid={ `player-score-${index}` }>{ ` - ${score} pontos` }</span>
+          </div>
+        )) }
       </main>
     );
   }
 }
 
-export default Ranking;
+const mapStateToProps = (state) => ({
+  state: state.player,
+});
+
+export default connect(mapStateToProps)(Ranking);
