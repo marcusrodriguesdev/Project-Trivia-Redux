e3c0825f5d159544a36e29af000183ffeeec761c
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-09-15 01:48:23 -0300

    CSS Pagina de Ranking

--- src/Pages/Ranking.js
+++ src/Pages/Ranking.js
@@ -5,20 +5,42 @@ import './ranking.css';
 
 class Ranking extends React.Component {
   render() {
+    const rank = JSON.parse(localStorage.getItem('ranking'));
+    rank.sort((a, b) => b.score - a.score);
     return (
       <div className="ranking-wrapper">
         <div className="rank-container">
-          <h3 className="rank-title">Ranking</h3>
-          <div className="rank-item">
-            <div className="rank-box-index">
-              <p className="rank-index">1</p>
+          <h3 className="rank-title" data-testid="ranking-title">Ranking</h3>
+          <span />
+          {rank.map((player, index) => (
+            <div className="rank-item" key={ index }>
+              <div className="rank-box-index">
+                <p className="rank-index">{ index + 1}</p>
+              </div>
+              <img className="rank-image" src={ player.picture } alt="Gravatar" />
+              <p
+                className="rank-player-name"
+                data-testid={ `player-name${index}` }
+              >
+                { player.name }
+              </p>
+              <img className="trophy" src={ trophy } alt="trophy" />
+              <p
+                className="rank-player-score"
+                data-testid={ `player-score${index}` }
+              >
+                { player.score }
+                {' '}
+                pontos
+              </p>
             </div>
-            <img className="rank-image" src="https://memegenerator.net/img/instances/85220527/no-consegue-n-.jpg" alt="" />
-            <p className="rank-player-name">Marcus Vinicius Mendes</p>
-            <img className="trophy" src={ trophy } alt="trophy" />
-            <p className="rank-player-score">400 pontos</p>
-          </div>
+          ))}
         </div>
+        <Link to="/">
+          <button className="ranking-home-button" type="button" data-testid="btn-go-home">
+            Home
+          </button>
+        </Link>
       </div>
     );
   }
