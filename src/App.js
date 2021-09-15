833fb914828801006e88cd9d38c31eaa7c39e60f
Marcos Estevao <dusdoom@gmail.com>
2021-09-14 23:25:59 -0300

    trying to fix lint problems again

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import Game from './pages/Game';
 import Config from './pages/Config';
 import Ranking from './pages/Ranking';
+import './App.css';
 
 export default function App() {
   return (
